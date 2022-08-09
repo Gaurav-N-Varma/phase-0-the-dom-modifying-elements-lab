2bae63002e8434c01c5247f626ddfda770f60ea8
Gaurav <gaurav.narayan.varma@gmail.com>
2022-08-09 17:33:14 -0400

    commit tree 309477e8c0ca243fe358422b70753379d459b836

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const main = document.getElementById('main');
 main.remove();
 
 const newHeader = document.createElement('h1');
-newHeader.setAttribute('id', 'victory');
+// newHeader.setAttribute('id', 'victory');
+newHeader.id = 'victory'
 newHeader.innerHTML = 'YOUR-NAME is the champion';
